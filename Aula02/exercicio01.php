07140ce9c6befb4355ad382c871e2e0634734ebc
Ana Beatriz Ribeiro Soares <159508471+Anabti@users.noreply.github.com>
2025-02-27 09:01:07 -0300

    Add files via upload

---
+++ Aula02/exercicio01.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = "Ana";
+    $idade = "17";
+    $salario = 1700;
+
+    echo "Olá! Qual o seu nome? <br>";
+    echo "$nome <br>";
+    echo "Oi $nome! Quantos anos você tem? <br>";
+    echo "$idade<br>";
+    echo "Então você tem $idade anos! <br>";
+    echo "Quanto você ganha $nome? <br>";
+    echo " $salario <br>";
+    echo "$salaro! Eu espero que seja por hora e não por ano! rsrsrs... <br>";
+
+?>
